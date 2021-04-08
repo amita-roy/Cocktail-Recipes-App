5fd296c88f35eb2e677d653c8a9ae6f243c1ca1e
Amita-Roy <sardana.amita2@gmail.com>
2021-04-08 21:10:25 +0200

    Add action for filtered cocktails

--- src/actions/cocktailsActions.js
+++ src/actions/cocktailsActions.js
@@ -1,9 +1,14 @@
 import ACTIONS from './actionTypes';
-import { fetchCocktails, fetchCocktail } from '../api/cocktailApi';
+import { fetchCocktails, fetchCocktail, fetchFilteredCocktails } from '../api/cocktailApi';
 
 export const loadCocktails = () => async (dispatch) => {
   const response = await fetchCocktails();
   dispatch({ type: ACTIONS.LOAD_COCKTAILS, payload: response });
+};
+
+export const loadFilteredCocktails = () => async (dispatch) => {
+  const response = await fetchFilteredCocktails();
+  dispatch({ type: ACTIONS.FILTERED_COCKTAILS, payload: response });
 };
 
 export const loadCocktail = (cocktail) => async (dispatch) => {
