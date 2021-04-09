545fc393ad171b7b9d98181e4ba8d721a2885cfa
Amita-Roy <sardana.amita2@gmail.com>
2021-04-09 16:46:37 +0200

    Update homepage

--- src/pages/homePage/Homepage.js
+++ src/pages/homePage/Homepage.js
@@ -1,8 +1,9 @@
 import React, { PureComponent } from 'react';
+import './Homepage.css';
 
 class Homepage extends PureComponent {
   render() {
-    return <div>Home</div>;
+    return <div className="homepage">Home</div>;
   }
 }
 
