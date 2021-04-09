ac52e40c25588c597680193917b180a2927ad723
Amita-Roy <sardana.amita2@gmail.com>
2021-04-09 09:47:07 +0200

    Add cocktail component

---
+++ src/components/cocktail/Cocktail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Cocktail = ({ cocktail }) => (
+  <div>
+    <h2>{cocktail.strDrink}</h2>
+  </div>
+);
+
+Cocktail.propTypes = {
+  cocktail: PropTypes.objectOf(
+    PropTypes.shape({
+      strDrink: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default Cocktail;
