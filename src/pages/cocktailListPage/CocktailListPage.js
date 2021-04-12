fcaaced595ba621a6a55a13d8a3d705ed53158e1
Amita-Roy <sardana.amita2@gmail.com>
2021-04-12 16:42:21 +0200

    Add inline disabling lint

--- src/pages/cocktailListPage/CocktailListPage.js
+++ src/pages/cocktailListPage/CocktailListPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Jumbotron from '../../components/jumbotron/Jumbotron';
-import CocktailList from '../../container/cocktailList/CocktailList';
-import CocktailsFilterForm from '../../container/cocktailsFilterForm/CocktailsFilterForm';
+import CocktailList from '../../container/cocktailList/CocktailList'; // eslint-disable-line import/no-named-as-default
+import CocktailsFilterForm from '../../container/cocktailsFilterForm/CocktailsFilterForm'; // eslint-disable-line import/no-named-as-default
 
 import './CocktailListPage.css';
 
-export const CocktailListPage = () => (
+const CocktailListPage = () => (
   <div>
     <Jumbotron />
     <CocktailsFilterForm />
